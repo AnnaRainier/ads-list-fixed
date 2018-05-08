aaf1a4f3df66d871b21d54e816c58ca9c425465f
Anna <annarainier11@gmail.com>
2018-05-08 16:27:57 +0300

    ads-list-fixed

---
+++ src/app/ads/ads-edit/ads-edit.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { Ad } from '../models/Ad';
+import { AdsService } from '../ads.service';
+import { User } from '../models/User';
+import { UsersService } from '../../app-common/users.service';
+
+@Component({
+  selector: 'app-ads-edit',
+  templateUrl: './ads-edit.component.html',
+  styleUrls: ['./ads-edit.component.css']
+})
+export class AdsEditComponent implements OnInit {
+  ad: Ad
+  user: User
+  currentUserId: number
+  adId: number
+  constructor(private adsService: AdsService,
+    private usersService: UsersService,
+    private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    this.route.paramMap.switchMap((params: ParamMap) => {
+      const id = +params.get('id');
+      if (this.checkForEdit(this.router.url)) {
+        this.adId = id;
+      } else this.adId = null;
+      return this.adsService.getById(id);
+    })
+    .subscribe((ad: Ad) => 
+  this.ad = ad
+);
+
+  this.user = this.usersService.getAuthorized();
+  
+  this.getCurrentUser();
+  }
+
+  checkForEdit(link) {
+    let str = link;
+    let pattern = '\/edit';
+    return (link.match(pattern) != null);
+  }
+
+  getCurrentUser () {
+    let currentUser = this.usersService.getAuthorized();
+    //console.log(currentUser);
+    if (currentUser.id) return this.currentUserId = currentUser.id; 
+  }
+
+}
---
+++ src/app/ads/ads.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { StoreService } from '../app-common/store.service';
+import { Observable } from 'rxjs/Observable';
+import { Ad } from './models/Ad'
+import { of } from 'rxjs/observable/of';
+import { LocalStorageStoreService } from '../app-common/local-storage-store.service';
+
+@Injectable()
+export class AdsService {
+
+  constructor(private store: StoreService, private localStorageStore: LocalStorageStoreService) { }
+
+  list() {
+    const list = this.localStorageStore.getList('ads') 
+      return of(list);
+  }
+
+  mock() {
+    return of(this.localStorageStore.setMock());
+  }
+
+ deleteAdItem(item): Observable<Ad[]> {
+   let list = this.localStorageStore.getList('ads');
+   let index;
+   console.log(list);
+   
+   for (let i = 0; i< list.length; i++) {
+     if(list[i].id == item.id) index = i;
+   }
+   list.splice(index, 1);
+   //console.log(item);
+   /*
+   list.map((ad, index) => {
+    if (ad.id == item.id) {
+      list.splice(index, 1);
+    }
+
+    
+   })
+     */ 
+   return this.localStorageStore.resetAds(list);
+  }
+
+  getById(id) {
+    let list = this.localStorageStore.getList('ads');
+    let result;
+    list.map((ad) => {
+      if ( ad.id == id) result = ad;
+    })
+    return of(result);
+  }
+
+  createNewAd(item, ad?) {
+    let user = this.localStorageStore.getList('userAuthorized');
+    let currentUserId;
+    let list = this.localStorageStore.getList('ads');
+    for (let i = 0; i < user.length; i++) {
+      currentUserId = user[i].id;
+    }
+    //let list = this.localStorageStore.getList('ads');
+    if (ad && currentUserId) {
+        if (ad.description && ad.title && currentUserId == ad.userId) {
+          item.id = ad.id;
+        item.title = ad.title;
+        item.description = ad.description;
+        item.userId = ad.userId;
+        item.authorName = ad.authorName;
+
+          for (let i = 0; i< list.length; i++) {
+            if(list[i].id == item.id) list.splice(i, 1);
+          }
+        if (list[list.length-1] !=undefined) {
+          item.id = list[list.length-1].id + 1;
+        } else item.id = 1;
+        list.push(item);
+        this.localStorageStore.resetAds(list); 
+      } else  {
+        alert ('either data is not valid or you have no rights');
+        //return of(list);
+      }
+    } else {
+        if (item.description && item.title && currentUserId!= undefined) {
+          if (list[list.length-1] !=undefined) {
+            item.id = list[list.length-1].id + 1;
+          } else item.id = 1;
+        let user = this.localStorageStore.getList('userAuthorized');
+        item.userId = this.localStorageStore.getList('userAuthorized')[0].id;
+        item.authorName = this.localStorageStore.getList('userAuthorized')[0].name;
+        list.push(item);
+        this.localStorageStore.resetAds(list);
+      } else {
+        alert ('either data is not valid or you have no rights');
+        //return of(list);
+      }
+        
+    }
+  }
+}
